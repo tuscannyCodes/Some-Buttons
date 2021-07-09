9f24118eb78c2d44f172ba63a3341f4dd0640da7
Tuscanny Polk <thomas@tuscannys-mbp.attlocal.net>
2021-07-09 01:27:41 -0400

    updating

---
+++ script.js
@@ -0,0 +1,55 @@
+//VARIABLES
+
+const button = document.getElementById("swipeClick");
+
+const hoverButton = document.getElementById("hoverMe");
+
+const dblButton = document.querySelector(".dblMe");
+
+//VARIABLES END
+
+
+// FUNCTIONS 
+
+function change (){
+
+    button.innerHTML= "Do it!"
+
+}
+
+function changeBack (){
+
+    button.innerHTML= " Click Me!"
+}
+
+
+function makeRed (){  
+
+    hoverButton.style.backgroundColor= "red"
+
+  }
+
+  function makeDefault(){
+
+    hoverButton.style.backgroundColor= "blueviolet"
+
+  }
+
+  function makeBlue (){
+   
+        dblButton.classList.toggle("dblMeDefault");
+    
+      
+  }
+
+ 
+
+
+  //EVENTLISTENER
+
+  hoverButton.addEventListener("mouseover",makeRed)
+
+ hoverButton.addEventListener("mouseout",makeDefault)
+
+  dblButton.addEventListener("dblclick",makeBlue)
+
